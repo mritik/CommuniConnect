2c6ce377e8936d3487aa859a0f38162ec6ff4aea
mritik <56925074+mritik@users.noreply.github.com>
2021-03-22 10:44:49 -0400

    Delete logout.js

--- logout.js
+++
@@ -1,9 +0,0 @@
-const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e) => {
-  e.preventDefault();
-  auth.signOut().then(() => {
-    console.log('user signed out');
-
-    window.location.href = "login.html";  
-  })
-});
